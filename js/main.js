abb37d2709cf16c75802abe5040173d7e64bf2bf
tschernysh <tschernysh@gmail.com>
2021-02-28 16:27:29 +0200

    works slider

--- js/main.js
+++ js/main.js
@@ -5,4 +5,15 @@ $(document).ready(function(){
         dots: true,
         dotsClass: 'slick__dots',
       });
+
+      $(".works__elements").slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        dotsClass: 'slick__dots_works',
+        slidesPerRow: 2,
+        variableWidth: true,
+        slidesToScroll: 1,
+      });
+
 })
